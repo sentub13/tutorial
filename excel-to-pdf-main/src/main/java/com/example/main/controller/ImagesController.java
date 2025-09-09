982dc68fc1d43c4a60f753956e3df731e96f2979
sentub13 <sentu.biswas@ust.com>
2025-09-09 15:43:00 +0530

    test

--- excel-to-pdf-main/src/main/java/com/example/main/controller/ImagesController.java
+++ excel-to-pdf-main/src/main/java/com/example/main/controller/ImagesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/product-images")
+@RequestMapping("/images")
 public class ImagesController {
     @Autowired
     private ImagesRepository imagesRepository;
