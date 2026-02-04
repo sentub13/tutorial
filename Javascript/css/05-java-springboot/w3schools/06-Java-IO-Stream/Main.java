a09ce23ebad9208968b9a082386903135c7231be
sentub13 <sentu.biswas@ust.com>
2026-02-04 18:30:57 +0530

    da

--- Javascript/css/05-java-springboot/w3schools/06-Java-IO-Stream/Main.java
+++ Javascript/css/05-java-springboot/w3schools/06-Java-IO-Stream/Main.java
@@ -1,50 +1,109 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Main {
 	public static void main(String[] args) {
 		// System.out.println("Hello World");
 		FileInputStreamApi obj1 = new FileInputStreamApi();
-		obj1.ReadFile();
-		obj1.CopyFile();
+		// obj1.ReadFile();
+		// obj1.CopyFile();
 		
 		FileOutputStreamApi obj2 = new FileOutputStreamApi();
-		obj2.WriteFile();
-		obj2.CopyFile();
+		// obj2.WriteFile();
+		// obj2.CopyFile();
 		obj2.AppendFile();
 		
 		BufferedReaderApi obj3 = new BufferedReaderApi();		
-		obj3.ReadFile();
+		// obj3.ReadFile();
 		
 		BufferedWriterApi obj4 = new BufferedWriterApi();
-		obj4.WriteFile();
-		obj4.AppendFile();
+		// obj4.WriteFile();
+		// obj4.AppendFile();
 	}
 }
 
 class FileInputStreamApi {
 	void ReadFile() {
-		System.out.println("Read input");
-		
+		// System.out.println("Read input");
+		// FileInputStream obj = new FileInputStream();
+		try (FileInputStream input = new FileInputStream("test.txt")) {
+			File file = new File("test.txt");
+			if(file.createNewFile()){
+				System.out.println("File created");
+			} else {
+				System.out.println("Not Created");
+			}
+			
+			int i;
+			while((i = input.read()) != -1) {
+				System.out.print((char) i);
+			}
+			
+			System.out.println("Try");
+		} catch(IOException e) {
+			System.out.println("Error read file.");
+		} 
 	}
 	
 	void CopyFile() {
-		System.out.println("Copy input");
+		// System.out.println("Copy input");
+		
+		try (FileInputStream input = new FileInputStream("test.txt"); FileOutputStream output = new FileOutputStream("test1.txt")) {
+			
+			int i;
+			while((i = input.read()) != -1) {
+				output.write(i);
+			}			
+			System.out.println("Copied successfully.");
+		} catch(IOException e) {
+			System.out.println("Error reading file.");
+		}
 	}
 }
 // ==================================
 class FileOutputStreamApi {
 	void WriteFile() {
 		System.out.println("Write output");
+		try (FileOutputStream obj = new FileOutputStream("test.txt")){
+			String str = "How are you";
+			obj.write(str.getBytes());
+			System.out.println("Succcesfully wrote the file");
+		} catch (IOException e) {
+			System.out.println("Error "+e);
+		}
 	}
 	
 	public void CopyFile() {
 		System.out.println("Copy output");
+		try (
+			FileInputStream input = new FileInputStream("test.txt"); 
+			FileOutputStream output = new FileOutputStream("hello.txt")
+		){
+			int b;
+			while ((b = input.read()) != -1) {
+				output.write(b);
+			}
+			System.out.println("File copied successfully");
+		} catch (IOException e) {
+			System.out.println("Error "+e);
+		}
 	}
 	
 	public static void AppendFile() {
 		System.out.println("Append output");
+		String str = "fffff";
+		try (FileOutputStream output = new FileOutputStream("hello.txt", true)){
+			output.write(str.getBytes());
+			
+			System.out.println("Text appended S
+			output.wucccesfully");
+		} catch (IOException e) {
+			System.out.println("Error "+e);
+		}
 	}
 }
 // ==================================
